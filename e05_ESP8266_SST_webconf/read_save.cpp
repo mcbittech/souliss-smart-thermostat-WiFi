f9dd7e00afedb1a51c9d4c76873de874a1a52059
Flavio <master.flavio.piccolo@gmail.com>
2016-02-17 10:24:28 +0100

    Offset for flash eeprom

--- e05_ESP8266_SST_webconf/read_save.cpp
+++ e05_ESP8266_SST_webconf/read_save.cpp
@@ -3,28 +3,26 @@
 #include "tools/store/store.h"          
 #include "read_save.h"
 
-
+int offset=100;
 
 void save_eeprom_byte(int index,byte value){ 
-  Store_8bit(index,value);
-  Serial.print("SaveEepromByte index");Serial.print(index);Serial.println(value);
+  Store_8bit(index+offset,value);
+  Serial.print("SaveEepromByte index");Serial.print(index+offset);Serial.println(value);
   Store_Commit();
 }
 
 void save_eeprom_int(int index,int value){ 
-  Store_16bit(index,value);
-  Serial.print("SaveEepromInt index");Serial.print(index);Serial.println(value);
+  Store_16bit(index+offset,value);
+  Serial.print("SaveEepromInt index");Serial.print(index+offset);Serial.println(value);
   Store_Commit();
 }
 
-
 void read_eeprom_byte(int index,int value){
-  value=Return_8bit(index);
-  Serial.print("readEepromByte index");Serial.print(index);Serial.println(value);
+  value=Return_8bit(index+offset);
+  Serial.print("readEepromByte index");Serial.print(index+offset);Serial.println(value);
 }
 
-
 void read_eeprom_int(int index,int value){
-  value=Return_16bit(index);
-  Serial.print("ReadEepromInt index");Serial.print(index);Serial.println(value);
+  value=Return_16bit(index+offset);
+  Serial.print("ReadEepromInt index");Serial.print(index+offset);Serial.println(value);
 }
