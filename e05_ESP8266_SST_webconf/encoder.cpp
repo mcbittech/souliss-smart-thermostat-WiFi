41c5d41c34c19f696942fba5e63a0cfd4d074eb9
mcbittech <master.flavio.piccolo@gmail.com>
2016-12-29 08:18:56 +0100

    more clean code encoder.cpp

--- e05_ESP8266_SST_webconf/encoder.cpp
+++ e05_ESP8266_SST_webconf/encoder.cpp
@@ -8,20 +8,15 @@ volatile float encoderValue = 20.0;
 int encoder_PinALast = LOW;
 
 
-
 void encoder() {
     int MSB = digitalRead(ENCODER_PIN_B); //MSB = most significant bit
     int LSB = digitalRead(ENCODER_PIN_A); //LSB = least significant bit
-
     int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
     int sum  = (encoder_PinALast << 2) | encoded; //adding it to the previous encoded value
-
     if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoder0Pos ++;
     if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoder0Pos --;
-  
     encoder_PinALast = encoded; //store this value for next time
     encoderValue = encoder0Pos / 10.0;
-
 }
 
 void setEncoderValue(float val) {
